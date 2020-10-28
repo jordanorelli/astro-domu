6960c3a587d18fcc2429164d085cf3859b63e629
Jordan Orelli <jordanorelli@gmail.com>
2020-10-28 01:22:57 +0000

    that was dumb

--- internal/wire/ok.go
+++ internal/wire/ok.go
@@ -4,8 +4,6 @@ type OK struct{}
 
 func (OK) NetTag() string { return "ok" }
 
-func (OK) MarshalJSON() ([]byte, error) { return []byte(`"ok"`), nil }
-
 func init() {
 	Register(func() Value { return new(OK) })
 }
